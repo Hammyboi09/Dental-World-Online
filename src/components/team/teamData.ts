42b16360b7d049beadf446eadee06091081ff8f7
Hammyboi09 <ihammadsyed.27@gmail.com>
2025-01-12 18:37:48 +0530

    add all changes

--- src/components/team/teamData.ts
+++ src/components/team/teamData.ts
@@ -6,7 +6,7 @@ export const teamMembers = [
     qualification: "BDS (KNP), Certificate of Implantology (Spain), Fellow ICOI",
     qualificationFull: "BDS (KNP), Certificate of Implantology (Spain), Fellow ICOI (International College of Oral Implantologists) Member, Indian Society of Oral Implantologists Member, Academy of Oral Implantology",
     about: "Dr. Deepak Nakra graduated from Rama Dental College Hospital & Research Center, Kanpur, in 2000. He established his first clinic, Dental World, in Rajouri Garden, New Delhi, and has been instrumental in the growth of the practice. He also set up a charitable dental clinic at Inderlok in memory of his parents, running since 2004. Dr. Nakra is actively involved in implant dentistry, having completed various implant courses in India and abroad. With years of experience in dental surgery, he handles complex surgeries with ease.",
-    image: "/elements/team/dr-deepak-nakra.jpg"
+    image: "/elements/team/dr-deepak-nakra.png"
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ export const teamMembers = [
     qualification: "BDS (Gold Medalist), Certificate of Endodontics (U.S.A.)",
     qualificationFull: "BDS (Gold Medalist), Certificate of Endodontics (U.S.A.), Ex Resident, RDC Hospital, Director, M.D. Hospital",
     about: "Dr. Harveen Nakra graduated from Rama Dental College Hospital & Research Center, Kanpur, in 2000. She directs Malhotra Dental Hospital and heads the dental clinic at Punjabi Bagh. Dr. Harveen Nakra joined Dental World in 2001. She has gained extensive experience in implant and cosmetic dentistry and holds a keen interest in orthodontics, completing further studies to expand her treatment offerings.",
-    image: "/elements/team/dr-harveen-nakra.jpg"
+    image: "/elements/team/dr-harveen-nakra.png"
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ export const teamMembers = [
     qualification: "BDS (Rajasthan Dental College and Hospital)",
     qualificationFull: "BDS (Rajasthan Dental College and Hospital)",
     about: "Dr. Tavleen Kaur completed her BDS from Rajasthan Dental College and Hospital in 2010. She has been an Associate Dentist at the Rajouri Garden branch of Dental World for over five years, excelling in all aspects of the practice. Her comprehensive approach ensures deep knowledge and understanding of dental care.",
-    image: "/elements/team/dr-tavleen-kaur.jpg"
+    image: "/elements/team/dr-tavleen-kaur.png"
   },
   {
     id: 4,
@@ -51,7 +51,7 @@ export const teamMembers = [
     qualification: "BDS, MDS in Endodontics and Conservative Dentistry",
     qualificationFull: "BDS (Rama Dental College, Kanpur), MDS in Endodontics and Conservative Dentistry (Darshan Dental College, Udaipur)",
     about: "Dr. Raman Kamboj is an expert endodontist specializing in root canal treatments, rotary endodontics, re-treatments, apicectomy, and surgical endodontic procedures. His extensive education and skills ensure effective and precise care for patients.",
-    image: "/elements/team/dr-raman-kamboj.jpg"
+    image: "/elements/team/dr-raman-kamboj.png"
   },
   {
     id: 7,
@@ -60,6 +60,6 @@ export const teamMembers = [
     qualification: "BDS, MDS (Subharti Dental College)",
     qualificationFull: "BDS (Subharti Dental College), MDS (Subharti Dental College)",
     about: "Dr. Pratik Aggarwal is a dynamic and skilled orthodontist serving as a consultant at SRG Hospital, Max Superspeciality Hospital, and Navjeevan Hospital in New Delhi. He frequently attends national and international conferences to stay updated with the latest advancements in orthodontics.",
-    image: "/elements/team/dr-prateek-agaarwal.jpg"
+    image: "/elements/team/dr-prateek-agaarwal.png"
   }
 ];
