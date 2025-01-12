c120cbe6c99367e91c7dbb8d384a3f103228c8eb
Hammyboi09 <ihammadsyed.27@gmail.com>
2025-01-12 13:50:47 +0530

    Your descriptive commit message

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NavBar } from './components/navigation/NavBar';
 import { Hero } from './components/Hero';
-import { Services } from './components/Services';
+import { Services } from './components/services';
 import { Testimonials } from './components/Testimonials';
 import { Footer } from './components/footer/Footer';
 import { TeamPage } from './pages/TeamPage';
