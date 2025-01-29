74738bcf8bf19fda54a2317d1c2cbbc85e4c1c70
Hammyboi09 <ihammadsyed.27@gmail.com>
2025-01-29 21:56:16 +0530

    make changes

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -3,14 +3,14 @@ export const certificates = [
     id: 1,
     name: "Membership Certificate from the International Congress of Oral Implantologists (ICOI)",
     about: "Issued by the ICOI, this certificate acknowledges that Dr. Deepak Nakra is a member in good standing, highlighting his dedication to advancing oral implantology worldwide. It is signed by the ICOI President and President-Elect.",
-    cardImage: "elements/certifications/cards/icoi-certificate.jpg",
+    cardImage: "elements/certifications/cards/icoi-certificate.png",
     modalImage: "elements/certifications/modal/icoi-certificate.jpg"
   },
   {
     id: 2,
     name: "Implant Prosthetic Section (IPS) of the ICOI Certificate",
     about: "This certificate recognizes Dr. Deepak Nakra as a member in good standing of the Implant Prosthetic Section (IPS) of the International Congress of Oral Implantologists (ICOI). It is endorsed by the IPS Board Member and ICOI President.",
-    cardImage: "elements/certifications/cards/implant-prosthetic-section-icoi.jpg",
+    cardImage: "elements/certifications/cards/implant-prosthetic-section-icoi.png",
     modalImage: "elements/certifications/modal/implant-prosthetic-section-icoi.jpg"
   },
   {
@@ -24,21 +24,21 @@ export const certificates = [
     id: 4,
     name: "Fellowship Certificate from the Academy of Oral Implantology",
     about: "This fellowship certificate, awarded to Dr. Deepak Nakra by the Academy of Oral Implantology, acknowledges that he has successfully met the board's criteria and has been conferred the title of 'Fellow'. This recognition was granted in New Delhi, India, on August 4, 2018.",
-    cardImage: "elements/certifications/cards/academy-of-oral-implantogy.jpg",
+    cardImage: "elements/certifications/cards/academy-of-oral-implantogy.png",
     modalImage: "elements/certifications/modal/academy-of-oral-implantogy.jpg"
   },
   {
     id: 5,
     name: "Certificate of Dental Surgery (Dr. B.R Ambedkar University, Agra)",
     about: "This certificate, issued by Dr. Bhimrao Ambedkar University, confirms that Harveen Malhotra successfully passed the 2000 examination from S.R.M. Dental College, Kanpur, and was conferred the degree of Bachelor of Dental Surgery. It is dated December 21, 2000.",
-    cardImage: "elements/certifications/cards/dr-harveen-certificate-in-dental-surgery..jpg",
+    cardImage: "elements/certifications/cards/dr-harveen-certificate-in-dental-surgery..png",
     modalImage: "elements/certifications/modal/dr-harveen-certificate-in-dental-surgery..jpg"
   },
   {
     id: 6,
     name: "Certificate of Dental Surgery (Rajasthan University of Health Sciences, Jaipur)",
     about: "This certificate, issued by RUHS Jaipur, attests that Tavleen Kaur qualified in August 2009 and was awarded the degree of Bachelor of Dental Surgery. It is dated February 2, 2015.",
-    cardImage: "elements/certifications/cards/dr-tavleen-certificate-in-dental-surgery.jpg",
+    cardImage: "elements/certifications/cards/dr-tavleen-certificate-in-dental-surgery.png",
     modalImage: "elements/certifications/modal/dr-tavleen-certificate-in-dental-surgery.jpg"
   }
 ];
